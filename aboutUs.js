874c7ae31cf65848128f165dd07d12f23d1d6de6
Abrar822 <abrarshekh822@gmail.com>
2025-07-01 16:54:05 +0530

    Add files via upload

---
+++ aboutUs.js
@@ -0,0 +1,25 @@
+let aboutUsBtn = document.querySelectorAll('.aboutUsBtn');
+let aboutUsContainer = document.querySelector('.about-us-container');
+aboutUsBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        for(let section of sections) {
+            if(getComputedStyle(section).display !== 'none' && !section.classList.contains('about-us-container')) {
+                section.style.top = '1000px';
+                setTimeout(() => {
+                    section.style.display = 'none';
+                }, 70);
+                // specific to explore-exos
+                if(section.classList.contains('explore-exos-container')) {
+                    exoSearchBar.style.display = 'flex';
+                    exoButtons.style.display = 'flex';
+                }
+            }
+            setTimeout(() => {
+                aboutUsContainer.style.display = 'flex';
+            }, 100)
+            setTimeout(() => {
+                aboutUsContainer.style.top = '70px';
+            }, 300);
+        }
+    })
+})
